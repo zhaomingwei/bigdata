42873e5500ba36a4e562209eebe0e3cf9f70eb03
zhaowei <874336775@qq.com>
2020-10-13 11:32:53 +0800

    调整

--- src/main/java/hoau/com/cn/service/job/StationDetailJob.java
+++ src/main/java/hoau/com/cn/service/job/StationDetailJob.java
@@ -1,7 +1,6 @@
 package hoau.com.cn.service.job;
 
 import hoau.com.cn.entity.StationDetail;
-import hoau.com.cn.service.AllJobSubmitter;
 import hoau.com.cn.service.format.FilterOutputFormat;
 import hoau.com.cn.service.mapper.StationDetailMapper;
 import hoau.com.cn.service.reducer.StationDetailReducer;
@@ -26,17 +25,17 @@ import java.util.Properties;
  */
 public class StationDetailJob {
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
-        JobConf conf = new JobConf(AllJobSubmitter.class);
+        JobConf conf = new JobConf(StationDetailJob.class);
 
-        InputStream in = AllJobSubmitter.class.getResourceAsStream("/conf.properties");
+        InputStream in = StationDetailJob.class.getResourceAsStream("/conf.properties");
         Properties properties = new Properties();
         properties.load(in);
         String diverClass = properties.getProperty("driver.class");
         String url = properties.getProperty("driver.url");
         String userName = properties.getProperty("driver.username");
         String passWord = properties.getProperty("driver.password");
-        String stationBaseDir = properties.getProperty("station.baseDir");
-        String output_name = properties.getProperty("station.outputName");
+        String stationBaseDir = properties.getProperty("station.detail.baseDir");
+        String output_name = properties.getProperty("station.detail.outputName");
 
         String date = DateUtils.formatDate(new Date(), "yyyyMMddHHmmss");
         String output_dir = date;
@@ -65,8 +64,8 @@ public class StationDetailJob {
                 "  LEFT JOIN HYDATA.LD_GS GS" +
                 "    ON GS.GSBH = FHD.SCGSBH" +
                 " WHERE Q.FCSJ >= to_date('2016-06-01', 'yyyy-MM-dd')" +
-                "   AND Q.FCSJ < to_date('2016-06-10', 'yyyy-MM-dd')" +
-                "   AND GS.GSJC = 'N上海'";
+                "   AND Q.FCSJ < to_date('2016-06-10', 'yyyy-MM-dd')";
+//                "   AND GS.GSJC = 'N上海'";
         String count = "SELECT COUNT(1)" +
                 "  FROM HYDATA.LD_FHD FHD" +
                 " INNER JOIN HYDATA.LD_FHCLQK Q" +
@@ -78,20 +77,22 @@ public class StationDetailJob {
                 "  LEFT JOIN HYDATA.LD_GS GS" +
                 "    ON GS.GSBH = FHD.SCGSBH" +
                 " WHERE Q.FCSJ >= to_date('2016-06-01', 'yyyy-MM-dd')" +
-                "   AND Q.FCSJ < to_date('2016-06-10', 'yyyy-MM-dd')" +
-                "   AND GS.GSJC = 'N上海'";
+                "   AND Q.FCSJ < to_date('2016-06-10', 'yyyy-MM-dd')";
+//                "   AND GS.GSJC = 'N上海'";
 
+        /**自定义的mapper输出类型的key、value类型要和自定义的reduce的输入的key、value类型对应上，这里都是Text, StationDetail**/
+        /**reduce最后要输出到自定义的文件里，类型也必须要和自定义的FilterRecordWrite类继承的RecordWriter<Text, Text>类型一致且重写的write方法类型也要保持一致,否则不能输出到文件**/
         /**Job任务**/
         Job job = new Job(conf, "GET_STATIONS_DETAIL");
-        /**设置输入格式为数据**/
+        /**设置输入格式为:从数据库获取数据**/
         job.setInputFormatClass(DBInputFormat.class);
-        /**设置map输出key、value的类型**/
+        /**设置map输出key、value的类型,即：自定义的StationDetailMapper中的定义的泛型Mapper<LongWritable, StationDetail, Text, StationDetail>后两个类型**/
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(StationDetail.class);
         /**设置mapper和reduce的处理类**/
         job.setMapperClass(StationDetailMapper.class);
         job.setReducerClass(StationDetailReducer.class);
-        /**设置reducer输出key的类型**/
+        /**设置reducer输出key的类型,即：自定义的StationDetailReducer中的定义的泛型Reducer<Text, StationDetail, Text, Text>后两个类型**/
         job.setOutputKeyClass(Text.class);
         /**设置输出文件格式，实现自定义文件名称**/
         job.setOutputFormatClass(FilterOutputFormat.class);
@@ -100,14 +101,13 @@ public class StationDetailJob {
 
         String baseDir = stationBaseDir + conf.get("output_dir");
 
-        /**获取当天时间,场站信息每天就跑一次**/
         Path path = new Path( baseDir + "/" + output_dir + "/" + output_name);
         FileSystem fs = path.getFileSystem(conf);
         if (!fs.exists(path)) {
-            System.out.println("当天第一次输出场站信息...");
+            System.out.println("开始生成场站明细信息...");
             FileOutputFormat.setOutputPath(job, new Path(baseDir));
         }else {
-            System.out.println("当天已输出场站信息,无需再次输出!");
+            System.out.println("场站明细信息输出路径已存在,无需再次输出!");
         }
 
         System.exit(job.waitForCompletion(true) ? 0 : 1);
